093fef2bf835eedc73ac3f376e99b1a41c7494a0
Arun K <arunreturns@gmail.com>
2016-03-23 16:38:30 +0000

    Removing clusters

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var Express = require('express');
 
-if ( process.env.NODE_ENV === 'production' ) {
+/*if ( process.env.NODE_ENV === 'production' ) {
     
     var Cluster = require('cluster');
 
@@ -54,10 +54,25 @@ if ( process.env.NODE_ENV === 'production' ) {
         console.log('Running in <%s>', process.env.NODE_ENV);
         console.log('Listening on port [%d]', devPort);
     });
-}
+}*/
 /**/
 
+var App = new Express();
+        
+var port = process.env.PORT || 8080; 
 
+require('./server/configServer.js')(App);
+
+require('./server/databaseConfig.js')(App);
+
+App.use(Express.static(__dirname + '/public'));
+App.set("appName", "file-storer.herokuapp.com");
+
+require('./server/expressRoutes.js')(App);
+
+App.listen(port, function() { 
+    console.log('Listening on port [%d]', port);
+});
 
 
 module.exports = App;
